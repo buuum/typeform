1962ade3c03fb72e4a23e856be2de98c58f49dbf
Ferran Martinez <alfonsmartinezparra@gmail.com>
2016-11-30 21:48:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,198 @@
+Buuum -Typeform
+=======================================
+
+[![Packagist](https://poser.pugx.org/buuum/typeform/v/stable)](https://packagist.org/packages/buuum/typeform)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000)](#license)
+
+## Simple and extremely flexible PHP event class
+
+## Getting started
+
+You need PHP >= 5.5 to use Buuum.
+
+- [Install Buuum typeform](#install)
+
+## Install
+
+### System Requirements
+
+You need PHP >= 5.5.0 to use Buuum\Typeform but the latest stable version of PHP is recommended.
+
+### Composer
+
+Buuum is available on Packagist and can be installed using Composer:
+
+```
+composer require buuum/typeform
+```
+
+### Manually
+
+You may use your own autoloader as long as it follows PSR-0 or PSR-4 standards. Just put src directory contents in your vendor directory.
+
+## Methods
+
+### getForm
+```php
+$typeform = new Typeform($api_key);
+$response = $typeform->getForm($token_form);
+```
+response
+```php
+array(3) {
+  ["stats"]=> array(3) {
+    ["showing"]=> int(1)
+    ["total"]=> int(48)
+    ["completed"]=> int(27)
+  }
+  ["questions"]=> array(9) {
+    [0]=> array(3) {
+      ["id"]=>
+      string(20) "list_24762704_choice"
+      ["question"]=>
+      string(4) "test"
+      ["field_id"]=>
+      int(24762704)
+    }
+    ... 
+  }
+  ["responses"]=>
+    array(5) {
+      ["completed"]=>
+      string(1) "1"
+      ["token"]=>
+      string(32) "bdc6d79bbf141af6b119f7bffde31bcb"
+      ["metadata"]=>
+      array(7) {
+        ["browser"]=>
+        string(7) "default"
+        ["platform"]=>
+        string(5) "other"
+        ["date_land"]=>
+        string(19) "2016-11-30 16:30:28"
+        ["date_submit"]=>
+        string(19) "2016-11-30 16:30:38"
+        ["user_agent"]=>
+        string(120) "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36"
+        ["referer"]=>
+        string(59) "https://kuvut1.typeform.com/to/A7moU3?hash=dsfsdf&quiz=3243"
+        ["network_id"]=>
+        string(10) "11ab630aa4"
+      }
+      ["hidden"]=>
+      array(2) {
+        ["hash"]=>
+        string(6) "dsfsdf"
+        ["quiz"]=>
+        string(4) "3243"
+      }
+      ["answers"]=>
+      array(6) {
+        ["list_24762704_choice"]=>
+        string(1) "1"
+        ["yesno_24736784"]=>
+        string(1) "0"
+        ["list_24817134_choice"]=>
+        string(2) "r2"
+        ["list_24817136_choice"]=>
+        string(2) "r2"
+        ["number_24818069"]=>
+        string(3) "423"
+        ["score"]=>
+        string(1) "3"
+      }
+    }
+    
+```
+
+### getForms
+```php
+$typeform = new Typeform($api_key, $url_typeform);
+$typeform->getForms();
+
+$typeform->getStats();
+$typeform->getQuestions();
+$typeform->getResponses();
+```
+
+### getAllForms
+```php
+$typeform = new Typeform($api_key, $url_typeform);
+$typeform->getAllForms();
+
+$typeform->getStats();
+$typeform->getQuestions();
+$typeform->getResponses();
+```
+
+### formatDataByExport
+```php
+$typeform = new Typeform($api_key, $url_typeform);
+$typeform->getAllForms();
+
+$data = $typeform->formatDataByExport();
+
+$fp = fopen('excel.csv', 'w');
+fputcsv($fp, $data['headers'], "\t");
+foreach ($data['rows'] as $row) {
+    fputcsv($fp, $row, "\t");
+}
+fclose($fp);
+
+```
+
+### getNextPage, getPrevPage
+```php
+$typeform->getNextPage();
+$typeform->getPrevPage();
+```
+
+## Setters
+
+```php
+
+$typeform->setLimit(10);
+$typeform->setApiKey($api_key);
+$typeform->setApiUri($api_uri);
+$typeform->setFormId($formId);
+$typeform->setFormIdFromUrl($url_typeform);
+$typeform->setPage($page);
+$typeform->setCompleted($completed);
+$typeform->setSince($since);
+$typeform->setUntil($until);
+
+```
+
+## Work with webhook
+webhook.php
+```php
+
+$typeform = new Typeform($api_key);
+$response = $typeform->getPayLoad();
+
+```
+
+response
+
+```php
+array(
+    'hiddens' => array(
+        'hash' => ''
+     ),
+    'score'   => 43,
+    'token'   => '4234324234332ewfwer3r2rfe'
+);
+```
+
+
+## LICENSE
+
+The MIT License (MIT)
+
+Copyright (c) 2016 alfonsmartinez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/typeform/Typeform.php
@@ -0,0 +1,566 @@
+<?php
+
+namespace Buuum;
+
+use Curl\Curl;
+
+class Typeform
+{
+
+    const MAX_LIMIT_RANGE = 1000;
+    /**
+     * API KEY
+     *
+     * @var string
+     */
+    protected $api_key;
+
+    /**
+     * API URI
+     *
+     * @var string
+     */
+    protected $api_uri = 'https://api.typeform.com/v1/form/';
+
+    /**
+     * form id
+     *
+     * @var int
+     */
+    protected $typeform_id;
+
+    /**
+     * Current page
+     *
+     * @var int
+     */
+    protected $page = 0;
+
+    /**
+     * Result limit
+     *
+     * @var int
+     */
+    protected $limit = 50;
+
+    /**
+     * Start date
+     *
+     * @var
+     */
+    protected $since = null;
+
+    /**
+     * End date
+     *
+     * @var
+     */
+    protected $until = null;
+
+    /**
+     * Whether or not to get completed results
+     *
+     * @var bool
+     */
+    protected $completed = true;
+
+    /**
+     * Questions
+     *
+     * @var []
+     */
+    protected $questions = [];
+
+    /**
+     * Responses
+     *
+     * @var []
+     */
+    protected $responses = [];
+
+    /**
+     * Statistics
+     *
+     * @var []
+     */
+    protected $stats = [];
+
+
+    /**
+     * @var Curl
+     */
+    protected $curl;
+
+    /**
+     * Typeform constructor.
+     * @param $api_key
+     * @param $url_typeform
+     */
+    public function __construct($api_key, $url_typeform = false)
+    {
+        $this->api_key = $api_key;
+        if ($url_typeform) {
+            $this->typeform_id = $this->getTypeformId($url_typeform);
+        }
+        $this->curl = new Curl();
+    }
+
+    /**
+     * @param $token
+     * @return array
+     * @throws \Exception
+     */
+    public function getForm($token)
+    {
+        $params = [
+            'key'   => $this->getApiKey(),
+            'token' => $token
+        ];
+
+        $data = $this->getHttpResponse($params);
+
+        return [
+            'stats'     => $data['stats']['responses'],
+            'questions' => $data['questions'],
+            'responses' => $data['responses'][0]
+        ];
+
+    }
+
+    /**
+     * @return $this|mixed
+     * @throws \Exception
+     */
+    public function getForms()
+    {
+        // Calculate offset
+        $offset = $this->getPage() * $this->getLimit();
+        $params = [
+            'key'       => $this->getApiKey(),
+            'offset'    => $offset,
+            'limit'     => $this->getLimit(),
+            'completed' => $this->getCompleted()
+        ];
+
+        if ($this->getSince()) {
+            $params['since'] = $this->getSince();
+        }
+        if ($this->getUntil()) {
+            $params['until'] = $this->getUntil();
+        }
+
+        $data = $this->getHttpResponse($params);
+
+        $this->initializeForm($data);
+
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function getAllForms()
+    {
+
+        $this->resetResults();
+
+        $previous_limit = $this->getLimit();
+        $this->setLimit(self::MAX_LIMIT_RANGE);
+
+        $this->getForms();
+        $total = ($this->getCompleted()) ? $this->stats['responses']['completed'] : $this->stats['responses']['total'];
+
+        while (count($this->getResponses()) < $total) {
+            $this->getNextPage();
+        }
+
+        $this->setLimit($previous_limit);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPayLoad()
+    {
+        $request_body = file_get_contents('php://input');
+        $data = json_decode($request_body, true);
+
+        return [
+            'hiddens' => isset($data['form_response']['hidden']) ? $data['form_response']['hidden'] : [],
+            'score'   => isset($data['form_response']['calculated']) ? $data['form_response']['calculated']['score'] : 0,
+            'token'   => $data['form_response']['token']
+        ];
+    }
+
+
+    /**
+     * @return array
+     */
+    public function formatDataByExport()
+    {
+        return [
+            'headers' => $this->getHeaders(),
+            'rows'    => $this->getRows()
+        ];
+    }
+
+
+    /**
+     * @return $this|Typeform|mixed
+     * @throws \Exception
+     */
+    public function getNextPage()
+    {
+        $this->nextPage();
+        return $this->getForms();
+    }
+
+    /**
+     * @return $this|Typeform|mixed
+     * @throws \Exception
+     */
+    public function getPrevPage()
+    {
+        $this->prevPage();
+        return $this->getForms();
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->api_key;
+    }
+
+    /**
+     * @param $api_key
+     * @return $this
+     */
+    public function setApiKey($api_key)
+    {
+        $this->api_key = $api_key;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiUri()
+    {
+        return $this->api_uri;
+    }
+
+    /**
+     * @param $api_uri
+     * @return $this
+     */
+    public function setApiUri($api_uri)
+    {
+        $this->api_uri = $api_uri;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFormId()
+    {
+        return $this->typeform_id;
+    }
+
+    /**
+     * @param $formId
+     * @return $this
+     */
+    public function setFormId($formId)
+    {
+        $this->resetResults();
+        $this->typeform_id = $formId;
+        return $this;
+    }
+
+    /**
+     * @param $url_typeform
+     * @return $this
+     */
+    public function setFormIdFromUrl($url_typeform)
+    {
+        $this->resetResults();
+        $this->typeform_id = $this->getTypeformId($url_typeform);
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param int $page
+     * @return $this
+     */
+    public function setPage($page)
+    {
+        if ($page < 0) {
+            $this->page = 0;
+        } else {
+            $this->page = $page;
+        }
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param $limit
+     * @return $this
+     */
+    public function setLimit($limit)
+    {
+        if ($limit > self::MAX_LIMIT_RANGE) {
+            throw new \InvalidArgumentException("Pagination defaults are 0 and 1000 responses/page (" . self::MAX_LIMIT_RANGE . " is the maximum allowed).");
+        }
+        $this->limit = $limit;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuestions()
+    {
+        return $this->questions;
+    }
+
+    /**
+     * @param $questions
+     * @return $this
+     */
+    protected function setQuestions($questions)
+    {
+        $this->questions = array_merge($this->questions, $questions);
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResponses()
+    {
+        return $this->responses;
+    }
+
+    /**
+     * @param $responses
+     * @return $this
+     */
+    protected function setResponses($responses)
+    {
+        $this->responses = array_merge($this->responses, $responses);
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStats()
+    {
+        return $this->stats;
+    }
+
+    /**
+     * @param mixed $stats
+     * @return Typeform
+     */
+    protected function setStats($stats)
+    {
+        $this->stats = $stats;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompleted()
+    {
+        return var_export($this->completed, true);
+    }
+
+    /**
+     * @param $completed
+     * @return $this
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSince()
+    {
+        return $this->since;
+    }
+
+    /**
+     * @param $since
+     * @return $this
+     */
+    public function setSince($since)
+    {
+        $this->since = $since;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUntil()
+    {
+        return $this->until;
+    }
+
+    /**
+     * @param $until
+     * @return $this
+     */
+    public function setUntil($until)
+    {
+        $this->until = $until;
+        return $this;
+    }
+
+    public function getUrlApi()
+    {
+        return $this->api_uri . $this->typeform_id;
+    }
+
+    /**
+     * @return Typeform
+     */
+    protected function nextPage()
+    {
+        $page = $this->getPage();
+        return $this->setPage(++$page);
+    }
+
+    /**
+     * @return Typeform
+     */
+    protected function prevPage()
+    {
+        $page = $this->getPage();
+        return $this->setPage(--$page);
+    }
+
+    /**
+     * @param $data
+     */
+    protected function initializeForm($data)
+    {
+        $questionArray = [];
+        foreach ($data['questions'] as $question) {
+            $questionArray[$question['id']] = $question['question'];
+        }
+        $this->setQuestions($questionArray);
+        $this->setResponses($data['responses']);
+        $this->setStats($data['stats']);
+    }
+
+    /**
+     * @param $typeform_url
+     * @return mixed
+     */
+    private function getTypeformId($typeform_url)
+    {
+        $re = "@/to/([^?]+)@";
+        preg_match($re, $typeform_url, $matches);
+        return $matches[1];
+    }
+
+    /**
+     * @param $params
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function getHttpResponse($params)
+    {
+        $this->curl->get($this->getUrlApi(), $params);
+
+        if ($this->curl->error) {
+            throw new \Exception('The supplied API key is not valid');
+        }
+
+        $data = json_decode($this->curl->rawResponse, true);
+
+        if (is_null($data)) {
+            throw new \Exception('The supplied API key is not valid');
+        }
+
+        return $data;
+
+    }
+
+    /**
+     * @return array
+     */
+    protected function getHeaders()
+    {
+        $headers = [];
+        foreach ($this->getQuestions() as $id => $question) {
+            $headers[] = $question;
+        }
+        $headers[] = 'score';
+
+        return array_map('utf8_decode', $headers);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getRows()
+    {
+        $rows = [];
+        foreach ($this->getResponses() as $response) {
+
+            $fields = [];
+            foreach ($this->getQuestions() as $question_id => $question_name) {
+                $rr = '';
+                if (isset($response['answers'][$question_id])) {
+                    $rr = $response['answers'][$question_id];
+                } elseif (isset($response['hidden'][$question_name])) {
+                    $rr = $response['hidden'][$question_name];
+                }
+                $fields[] = $rr;
+            }
+
+            $fields[] = isset($response['answers']['score']) ? $response['answers']['score'] : 0;
+            $rows[] = array_map('utf8_decode', $fields);
+        }
+
+        return $rows;
+    }
+
+    /**
+     *
+     */
+    public function resetResults()
+    {
+        $this->responses = [];
+        $this->questions = [];
+        $this->stats = [];
+    }
+}
